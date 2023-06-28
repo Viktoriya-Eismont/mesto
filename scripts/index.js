b244415551f699233b5ff24242bc64ba5af7e37c
Viktoriya-Eismont <vveismont@yandex.ru>
2023-06-28 10:44:52 +0300

    feat: разработать одностраничный сайт с 1 попапом

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const profileEditName = document.querySelector('.profile_edit_name');
+const profileInfo = document.querySelector('.profile__info');
+const popupTypeProfile = document.querySelector('.popup_type_profile');
+const buttonClosePopupEditProfile = popupTypeProfile.querySelector('.popup__close');
+const inputName = popupTypeProfile.querySelector('.popup_type_name');
+const inputAbout = popupTypeProfile.querySelector('.popup_type_about');
+const profileName = document.querySelector('.profile__name-edit');
+const profileTitle = profileName.querySelector('.profile__title');
+const profileSubtitle = profileInfo.querySelector('.profile__subtitle');
+const popupContent = popupTypeProfile.querySelector('.popup__content');
+
+function togglePopup() {
+  popupTypeProfile.classList.toggle('popup_opened');
+  inputName.value = profileTitle.textContent;
+  inputAbout.value = profileSubtitle.textContent;
+}
+
+profileEditName.addEventListener('click', togglePopup);
+buttonClosePopupEditProfile.addEventListener('click', togglePopup);
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = inputName.value;
+  profileSubtitle.textContent = inputAbout.value;
+}
+
+popupContent.addEventListener('submit', handleFormSubmit); 
+
